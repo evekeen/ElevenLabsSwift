1d5fc0b59a320e571e34f25e35ad12c036f9e06d
Alexander Ivkin <a@ivkin.dev>
2025-03-04 17:47:49 +0100

    try spokenAudio

--- Sources/ElevenLabsSwift/ElevenLabsSwift.swift
+++ Sources/ElevenLabsSwift/ElevenLabsSwift.swift
@@ -1180,7 +1180,7 @@ public class ElevenLabsSDK {
         do {
             // Configure for voice chat with minimum latency
             try audioSession.setCategory(.playAndRecord,
-                                         mode: .voiceChat,
+                                         mode: .spokenAudio,
                                          options: [.allowBluetooth])
 
             // Set preferred IO buffer duration for lower latency
